d91c8013a927b1b5d1da2cd3fce62fcfa8e3611f
WILSON PARDI JUNIOR <wilsonjr@gol.com>
2019-06-10 10:04:02 +0900

    Create solution.c
    My submitted solution to the "Pointers in C" problem.

---
+++ Easy/C/PointersInC/solution.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void update(int *a, int *b) 
+{
+        // Complete this function:
+        int c = 0;
+        int d = 0;
+    
+        c = *a + *b;
+        d = *a - *b;
+    
+        if(d < 0)
+                d = -d;
+    
+        *a = c;
+        *b = d;
+}
+
+int main() 
+{
+        int a, b;
+        int *pa = &a, *pb = &b;
+    
+        scanf("%d %d", &a, &b);
+    
+        update(pa, pb);
+    
+        printf("%d\n%d", a, b);
+
+        return 0;
+}
