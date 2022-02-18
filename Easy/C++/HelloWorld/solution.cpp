56e1d9eec5c6432c4a6af85db0a3b8e25915ca9a
WILSON PARDI JUNIOR <wilsonjr@gol.com>
2022-02-18 16:57:34 +0900

    Create solution.cpp

---
+++ Easy/C++/HelloWorld/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() 
+{
+    /* Enter your code here. Print output to STDOUT */ 
+    cout << "Hello, World!";
+    
+    return 0;
+}
