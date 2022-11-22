bc2e582185b9227df6b79357f054cf58485476cb
WILSON PARDI JUNIOR <wilsonjr@gol.com>
2022-11-22 18:02:44 +0900

    Create solution.c

---
+++ Medium/1D_ArraysInC/solution.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int i, n, sum = 0;
+	
+	scanf("%d", &n);
+	
+	int *arr = (int *)malloc(n*sizeof(int));
+	
+	for(i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+		
+		sum = sum + arr[i];
+	}
+	
+	printf("%d\n", sum);
+	
+	free(arr);
+	
+	return 0;
+}
+
